4d40d3cdd80217d22a6c17372d2e092b600a9f92
vcncolin <vcncolin@gmail.com>
2024-05-18 19:44:12 +0200

    size of graphs sucks

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Set the sidebar width
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+
 # Load your data
 df = pd.read_csv('data.csv')
 
@@ -18,7 +21,11 @@ df_filtered = df[(df['Année'] >= year_range[0]) & (df['Année'] <= year_range[1
 pie_topic = px.pie(df_filtered, values='Volume', names='Sous-groupe', title="Domaine enseigné")
 pie_genre = px.pie(df_filtered, values='Volume', names='Niveau', title="Niveau d'études")
 
+# Set the layout of the pie charts
+pie_topic.update_layout(width=300, height=300, margin=dict(l=20, r=20, t=20, b=20))
+pie_genre.update_layout(width=300, height=300, margin=dict(l=20, r=20, t=20, b=20))
+
 # Display pie charts side by side
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([2, 2])
 col1.plotly_chart(pie_topic)
 col2.plotly_chart(pie_genre)
