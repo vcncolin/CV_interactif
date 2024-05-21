af66957842df2f1572257aa0538622fa0795ce80
vcncolin <vcncolin@gmail.com>
2024-05-21 18:56:56 +0200

    width fix (wrong place for container size)

--- app_streamlit.py
+++ app_streamlit.py
@@ -19,8 +19,8 @@ field = st.multiselect("Domaine d'enseignement", df['Groupe'].unique(), default=
 df_filtered = df[(df['Année'] >= year_range[0]) & (df['Année'] <= year_range[1]) & df['Groupe'].isin(field)]
 
 # Create pie charts
-pie_topic = px.pie(df_filtered, values='Volume', names='Sous-groupe', title="Matière enseignée", use_container_width = True)
-pie_genre = px.pie(df_filtered, values='Volume', names='Niveau', title="Niveau universitaire", use_container_width = True)
+pie_topic = px.pie(df_filtered, values='Volume', names='Sous-groupe', title="Matière enseignée")
+pie_genre = px.pie(df_filtered, values='Volume', names='Niveau', title="Niveau universitaire")
 
 # Set the layout of the pie charts
 pie_topic.update_layout(margin=dict(l=20, r=20, t=20, b=20))
@@ -28,7 +28,7 @@ pie_genre.update_layout(margin=dict(l=20, r=20, t=20, b=20))
 
 # Display pie charts side by side
 
-st.plotly_chart(pie_topic)
-st.plotly_chart(pie_genre)
+st.plotly_chart(pie_topic, use_container_width=True)
+st.plotly_chart(pie_genre, use_container_width=True)
 
 st.dataframe(df_filtered)
