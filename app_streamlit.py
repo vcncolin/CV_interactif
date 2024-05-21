4c1de82e4918651e6bf33f3667fab9b139f0128a
vcncolin <vcncolin@gmail.com>
2024-05-21 18:44:42 +0200

    Modified graphs not side by side Added a dataframe at the bottom Button to select filed of study

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,29 +3,32 @@ import pandas as pd
 import plotly.express as px
 
 # Set the sidebar width
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
 # Load your data
 df = pd.read_csv('data.csv')
 
 # Add a title
-st.title('Interactive Dashboard for Classes Taught')
+st.title("Vincent COLIN - Domaines d'enseignement")
 
 # Add a slider in the sidebar
-year_range = st.sidebar.slider('Select Year Range', min(df['Année']), max(df['Année']), value=[min(df['Année']), max(df['Année'])])
+year_range = st.slider('Select Year Range', min(df['Année']), max(df['Année']), value=[min(df['Année']), max(df['Année'])])
+field = st.multiselect("Domaine d'enseignement", df['Groupe'].unique())
 
 # Filter data based on selected year range
-df_filtered = df[(df['Année'] >= year_range[0]) & (df['Année'] <= year_range[1])]
+df_filtered = df[(df['Année'] >= year_range[0]) & (df['Année'] <= year_range[1]) & df['Groupe'].isin(field)]
 
 # Create pie charts
-pie_topic = px.pie(df_filtered, values='Volume', names='Sous-groupe', title="Domaine enseigné")
-pie_genre = px.pie(df_filtered, values='Volume', names='Niveau', title="Niveau d'études")
+pie_topic = px.pie(df_filtered, values='Volume', names='Sous-groupe', title="Matière enseignée")
+pie_genre = px.pie(df_filtered, values='Volume', names='Niveau', title="Niveau universitaire")
 
 # Set the layout of the pie charts
-pie_topic.update_layout(width=300, height=300, margin=dict(l=20, r=20, t=20, b=20))
-pie_genre.update_layout(width=300, height=300, margin=dict(l=20, r=20, t=20, b=20))
+pie_topic.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+pie_genre.update_layout(margin=dict(l=20, r=20, t=20, b=20))
 
 # Display pie charts side by side
-col1, col2 = st.columns([2, 2])
-col1.plotly_chart(pie_topic)
-col2.plotly_chart(pie_genre)
+
+st.plotly_chart(pie_topic)
+st.plotly_chart(pie_genre)
+
+st.dataframe(df_filtered)
